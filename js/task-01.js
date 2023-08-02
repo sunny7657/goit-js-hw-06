d9ddbd114472ebbe04f85936865846884bbd8233
sunny7657 <ksvetlana466@gmail.com>
2023-08-02 18:29:23 +0200

    start

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+const list = document.querySelector('#categories');
+console.dir(list);
+
+console.dir(`Number of categories: ${list.children.length}`);
+
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+const item = document.querySelectorAll('.item');
+console.dir(item);
+
+item.forEach();
